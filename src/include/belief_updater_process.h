7a29137e5364dd49713260d8d1895ef11387ae73
Guillermo De Fermin <gdefermin@gmail.com>
2017-07-24 17:06:34 +0200

    Added license header

--- src/include/belief_updater_process.h
+++ src/include/belief_updater_process.h
@@ -1,3 +1,24 @@
+/*!*********************************************************************************
+ *  \file       belief_updater_process.h
+ *  \brief      BeliefUpdaterProcess definition file.
+ *  \details    This file contains the BeliefUpdaterProcess declaration. To obtain more information about
+ *              it's definition consult the belief_updater_process.cpp file.
+ *  \authors    Guillermo De Fermin
+ *  \copyright  Copyright 2016 Universidad Politecnica de Madrid (UPM)
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see http://www.gnu.org/licenses/.
+ ********************************************************************************/
 #ifndef BELIEF_UPDATER_PROCESS_H
 #define BELIEF_UPDATER_PROCESS_H
 
